5c3932404eefeb5912448dbf1fa25b93b812e547
MSI\justi <justin931027@gmail.com>
2026-07-02 16:39:41 +0800

    Add 0702 Java practice

---
+++ 0702/MyScoreAverage.java
@@ -0,0 +1,11 @@
+public class MyScoreAverage {
+    public static void main(String[] args) {
+        final int java = 85;
+        final int english = 78;
+        final int math = 92;
+
+        double average = (java + english + math) / 3.0;
+
+        System.out.println("Average score: " + average);
+    }
+}
---
+++ 0702/ProfileCard.java
@@ -0,0 +1,14 @@
+public class ProfileCard {
+    public static void main(String[] args) {
+        // 這個程式會輸出一張個人名片
+        String name = "Amy";
+        String department = "Information Management";
+        String favoriteCourse = "Programming";
+
+        System.out.println("=== Profile ===");
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        // 這一行會輸出最喜歡的課程內容
+        System.out.println("Favorite Course: " + favoriteCourse);
+    }
+}
